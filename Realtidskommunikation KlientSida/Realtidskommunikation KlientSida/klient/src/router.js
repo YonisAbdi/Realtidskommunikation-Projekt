93ce7550091b0f7a2c0b34f40b90f745042619f4
Yonis Abdi <yonisabdi.jobb@gmail.com>
2024-10-16 11:48:09 +0200

    Väder prognos API

--- Realtidskommunikation KlientSida/Realtidskommunikation KlientSida/klient/src/router.js
+++ Realtidskommunikation KlientSida/Realtidskommunikation KlientSida/klient/src/router.js
@@ -1,23 +1,15 @@
 ﻿import { createRouter, createWebHistory } from 'vue-router';
 import ChatComponent from './components/ChatComponent.vue';
-import WeatherComponent from './components/WeatherComponent.vue'; 
+import WeatherComponent from './components/WeatherComponent.vue';
 
 const routes = [
-    {
-        path: '/chat',
-        name: 'Chat',
-        component: ChatComponent,
-    },
-    {
-        path: '/weather',
-        name: 'Weather',
-        component: WeatherComponent,
-    },
+    { path: '/chat', component: ChatComponent },
+    { path: '/weather', component: WeatherComponent }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes
 });
 
 export default router;
